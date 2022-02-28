ffab64953d0279788360687d3ebae84dec4968a0
Alexis <alexis.o.santos@live.com>
2022-02-28 16:17:36 -0300

    criando class Livro

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -7,8 +7,48 @@ public class primeiroprograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro Livro1 = new Livro("O problema dos 3 corpos",300);
+        System.out.println(Livro1);
+
+
+
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World" + (a+b));*/
+    }
+}
+
+class Livro {
+    private string nome;
+    private integer numpaginas;
+
+    public Livro(string nome, integer numpaginas) {
+        this.nome = nome;
+        this.numpaginas = numpaginas;
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public integer getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(integer numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome=" + nome +
+                ", numpaginas=" + numpaginas +
+                '}';
     }
 }
